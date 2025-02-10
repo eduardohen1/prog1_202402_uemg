11885db98791c17b037c363811b5b71f211ce0b9
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2025-02-10 19:38:42 -0300

    Dados de conexao e funções de BD

--- controller/ConexaoMySQL.cs
+++ controller/ConexaoMySQL.cs
@@ -1,4 +1,5 @@
-﻿using MySql.Data.MySqlClient;
+﻿using FormsCadastro.model;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,46 @@ namespace FormsCadastro.controller
     public class ConexaoMySQL
     {
         MySqlConnection conexao = new MySqlConnection();
+        DadosConexao dadosConexao;
+
+        public ConexaoMySQL(DadosConexao dadosConexao)
+        {
+            this.dadosConexao = dadosConexao;
+        }
+
+        public bool conectar()
+        {
+            if (dadosConexao != null)
+            {
+                try
+                {
+                    string strConexao = "Server=" + dadosConexao.servidor + ";" +
+                                        "Database=" + dadosConexao.nomeBD + ";" +
+                                        "Uid=" + dadosConexao.usuario + ";" +
+                                        "Pwd=" + dadosConexao.senha + ";" +
+                                        "Connection Timeout=900;SSL Mode=Required;" +
+                                        "Port=" + dadosConexao.porta;
+                    conexao = new MySqlConnection(strConexao);
+                    conexao.Open();
+                    return true;
+                }catch(Exception ex)
+                {
+                    MessageBox.Show("Erro ao conectar no banco de dados:\n" +
+                                    ex.ToString(),
+                                    "Título do APP",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+                    return false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Dados de conexão com o banco de dados vazio. Verifique!",
+                                    "Título do APP",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+                return false;
+            }
 
     }
 }
---
+++ model/DadosConexao.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormsCadastro.model
+{
+    public class DadosConexao
+    {
+        public string servidor { get; set; }
+        public string usuario { get; set; }
+        public string senha { get; set; }
+        public string nomeBD { get; set; }
+        public int porta { get; set; }
+
+        //construtor:
+        public DadosConexao(string servidor, string usuario, string senha, 
+            string nomeBD, int porta)
+        {
+            this.servidor = servidor;
+            this.usuario = usuario;
+            this.senha = senha;
+            this.nomeBD = nomeBD;
+            this.porta = porta;
+        }
+
+        public DadosConexao(string servidor, string usuario, string senha,
+            string nomeBD)
+        {
+            this.servidor = servidor;
+            this.usuario = usuario;
+            this.senha = senha;
+            this.nomeBD = nomeBD;
+            this.porta = 3306;
+        }
+
+        //métodos:
+        public string ToString()
+        {
+            return "Servidor: "   + this.servidor + "\n" +
+                   "Usuário: "    + this.usuario  + "\n" +
+                   "Senha: "      + this.senha    + "\n" +
+                   "Nome do BD: " + this.nomeBD   + "\n" +
+                   "Porta: "      + this.porta;
+        }
+
+    }
+}
